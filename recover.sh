437248ef2d5b46c98049c5799c29bc7c963d25de
Herwig Bogaert <herwig.bogaert@viaa.be>
2017-12-12 22:35:01 +0100

    add excludefile

--- recover.sh
+++ recover.sh
@@ -6,13 +6,14 @@ Pool="$1"
 pgrep -x recover >/dev/null && echo "$(date '+%m/%d %H:%M:%S'): anoher recovery session is runnig, waiting..."
 while pgrep -x recover >/dev/null ; do
     sleep 10
-done 
+done
 
 read -r JsonObject
-Host=$(echo $JsonObject | jq -e .client)  || exit 2
-File=$(echo $JsonObject | jq -e .path) || exit 3
-Uid=$(echo $JsonObject | jq -e .uid) 
-Time=$(echo $JsonObject | jq -e .time)
+Host=$(echo $JsonObject | jq -er .client)  || exit 2
+File=$(echo $JsonObject | jq -er .path) || exit 3
+Uid=$(echo $JsonObject | jq -er .uid)
+Time=$(echo $JsonObject | jq -er .time)
+Exclude=$(echo $JsonObject | jq -er .exclude)
 
 echo "$(date '+%m/%d %H:%M:%S'): starting recovery $Host $File $Uid $Time"
 
@@ -27,20 +28,24 @@ Destination=$RECOVERY_AREA/$Host
 RecoverOptions=( -iY -a "-c $Host" "-d $Destination" )
 [ -n "$Pool" ] && RecoverOptions+=("-b $Pool")
 
-if [ "$Time" != "null" ]; then
-    RecoverOptions+=("-t '$(date -d $Time +%m/%d/%Y\ %H:%M:%S)'") 
+if [ "$Time" != 'null' ]; then
+    RecoverOptions+=("-t '$(date -d $Time +%m/%d/%Y\ %H:%M:%S)'")
     [ $? -eq 0 ] || exit 4
 fi
 
-[ -r $Destination/exclude.lst ] && RecoverOptions+=("-e $Destination/exclude.lst")
+if [ "$Exclude" != 'null' ]; then
+  ExcludeFile="/tmp/$$.$RANDOM"
+  echo $Exclude | jq -er '.[]' >$ExcludeFile
+  RecoverOptions+=("-e $ExcludeFile")
+fi
 
-echo eval recover ${RecoverOptions[@]} $File
+eval recover ${RecoverOptions[@]} $File
 RC=$?
 # treat non-zero rc as warning, because it may be harmless
 # for example, files that grew during backup
 [ $RC -ne 0 ] && echo "Warning: recover ended with non-zero rc: $RC"
 
-[ -n "$Uid" ] && [ -e "$Destination/$Basename" ] && chown -R  $Uid $Destination/$Basename
+[ "$Uid" != "null" ] && [ -e "$Destination/$Basename" ] && chown -R  $Uid $Destination/$Basename
 
 # If recoverd file is a symlink, also recover the file it points to
 if [ -L "$Destination/$Basename" ]; then
@@ -51,3 +56,4 @@ if [ -L "$Destination/$Basename" ]; then
   [ "$Uid" != "null" ] && [ -e "$Destination/$Target" ] && chown -R  $Uid $Destination/$Target
 fi
 
+[ -n "$ExcludeFile" ] && rm $ExcludeFile

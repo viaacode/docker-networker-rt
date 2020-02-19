a68e8641d864535539f126bb9a5a43def9063f0e
Herwig Bogaert <herwig.bogaert@viaa.be>
2020-02-19 14:40:44 +0100

    set time to most recent version in specified pool

--- recover.sh
+++ recover.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
+set -x
 # RecoveryArea must be set
 [ -z "$RecoveryArea" ] && exit 1
+# This command can be used to recover files in a disaster recovery environment.
+# Such an environment typically does not have access to all storage pools.
+# When the Pool parameter is sepcified, recovery will be restricted to 
+# this Pool.
 Pool="$1"
 
 read -r JsonObject
@@ -23,11 +28,26 @@ Destination=$RecoveryArea/$Host
 [ $(stat -c %a $Destination) -eq 0770 ] || chmod 0770 $Destination
 
 RecoverOptions=( -iY -a "-c $Host" "-d $Destination" )
-[ -n "$Pool" ] && RecoverOptions+=("-b $Pool")
+if [ -n "$Pool" ]; then
+    # Adding the -b option to the recover command will make it fail immediately
+    # when a file is not found in the specified Pool, rather than waiting for a
+    # pool that is not available in the recovery environemnt.  
+    RecoverOptions+=("-b $Pool")
+    # Even when the -b option is set, the recover command will try to restore
+    # from another pool if that pool contains a more recent version.
+    # Setting Time to the time of the most recent saveset in our Pool avoids
+    # recovery failure due to other pools, not available in the recovery
+    # environment, containing a more recent version of the file to be restored.
+    if [ -z "$Time" -o "$Time" == 'null' ]; then
+        Time=$(mminfo -c $Host -ot -q "pool=$Pool" -r  'savetime(17)' | tail -1)
+        echo "Setting recovery time to the savetime of the most recent saveset in pool $Pool: $Time"
+    fi
+fi
 
-if [ "$Time" != 'null' ]; then
-    RecoverOptions+=("-t '$(date -d $Time +%m/%d/%Y\ %H:%M:%S)'")
+if [ -n "$Time" -a "$Time" != 'null' ]; then
+    RecoverOptions+=("-t '$(date -d "$Time" +%m/%d/%Y\ %H:%M:%S)'")
     [ $? -eq 0 ] || exit 4
+    echo "Setting recovery time to: $Time"
 fi
 
 if [ "$Exclude" != 'null' ]; then

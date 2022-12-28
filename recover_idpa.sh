1db10bfd4f32d87f9bf5fd3779ef460343d25944
Herwig Bogaert <herwig.bogaert@viaa.be>
2022-12-28 23:06:17 +0100

    fix file lookup

--- recover_idpa.sh
+++ recover_idpa.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
+set -x
 
 # RecoveryArea must be set
 [ -z "$RecoveryArea" ] && exit 1
+[ -z "$AvamarUser" ] && exit 7
+[ -z "$AvamarPassword" ] && exit 8
 
 read -r JsonObject
 Host=$(echo $JsonObject | jq -er .client)  || exit 2
@@ -13,6 +16,9 @@ Exclude=$(echo $JsonObject | jq -er .exclude)
 echo "$(date '+%m/%d %H:%M:%S'): starting recovery $Host $File $Uid $Time"
 
 [ -z "$Host" ] || [ -z "$File" ] && exit 2
+
+# Do not restore nukk
+[ "$File" == "null" ] && exit 0
 
 echo $Host $File $Uid $Time
 
@@ -45,36 +51,45 @@ function findbackup(){
   Basename=$(basename $FileToFind)
   echo "searching" $FileToFind
 
-  BackupIds=( $(avtar --backups --server=do-mgm-idpa-ava.do.viaa.be --id=$AvamarUser --ap=$AvamarPassword --account=/clients/$Host $Before | tr -s ' ' | cut -d ' ' -f 4))
-
-  # Remove first 2 lines of output
-  unset BackupIds[0]
-  unset BackupIds[1]
-
   BackupToRestore=-1
+  #Target=( $(avtar --backups --server=do-mgm-idpa-ava.do.viaa.be --id=$AvamarUser --ap=$AvamarPassword --account=/clients/$Host $Before | sed -r 's/[0-9-]+ +[[0-9:]+ +([0-9]+).*(Linux|Unix) +([^ ]+) +([^ ]+).*/\1 \3 \4/' | egrep "[ ,]${FileToFind%/}(,|$)" | head -1) )
 
-  echo $RecoverOptions
+  Target=( $(avtar --backups --server=do-mgm-idpa-ava.do.viaa.be --id=$AvamarUser --ap=$AvamarPassword --account=/clients/$Host $Before | head -15 | sed -r 's/[0-9-]+ +[[0-9:]+ +([0-9]+).*(Linux|Unix) +([^ ]+) +([^ ]+).*/\1 \3 \4/' | while read id wd tgt; do expr "$tgt" : '/' >/dev/null && echo "$id $tgt" || echo "$id $wd/$tgt"; done | egrep "[ ,]${FileToFind%/}(,|$)" | head -1) )
+  if [ -n "$Target" ]; then
+      WorkDir=$(avtar --backups --server=do-mgm-idpa-ava.do.viaa.be --id=$AvamarUser --ap=$AvamarPassword --account=/clients/$Host --labelnum=$Target | sed -rn "s/ *[0-9-]+ +[[0-9:]+ +$Target .*(Linux|Unix) +([^ ]+) +([^ ]+).*/\2/p")/
+      FileToRestore=${FileToFind#$WorkDir}
+      BackupToRestore=$Target
+  else
+      FileToRestore="${FileToFind}"
+      BackupIds=( $(avtar --backups --server=do-mgm-idpa-ava.do.viaa.be --id=$AvamarUser --ap=$AvamarPassword --account=/clients/$Host $Before | tr -s ' ' | cut -d ' ' -f 4))
 
-  for id in ${BackupIds[@]} 
-  do
-      echo "Searching in backup $id"
-      BackupContent=( $(eval avtar --list --labelnum=$id --id=$AvamarUser --ap=$AvamarPassword --acnt=/clients/$Host --quiet) )
-      for content in ${BackupContent[@]}
-      do 
-          if [ $content = "$FileToFind" ]; then
-              echo "Found $File in $id"
-              BackupToRestore=$id
-              break 2
-          fi
+      # Remove first 2 lines of output
+      unset BackupIds[0]
+      unset BackupIds[1]
+
+
+      for id in ${BackupIds[@]} 
+      do
+          echo "Searching in backup $id"
+          BackupContent=( $(eval avtar --list --labelnum=$id --id=$AvamarUser --ap=$AvamarPassword --acnt=/clients/$Host --quiet) )
+          for content in ${BackupContent[@]}
+          do 
+              if [ $content = "$FileToFind" ]; then
+                  echo "Found $File in $id"
+                  BackupToRestore=$id
+                  break 2
+              fi
+          done
       done
-  done
+  fi
+
   if [[ $BackupToRestore -gt -1 ]]; then
       echo "Restoring backup with id $BackupToRestore"
       if [[ "$FileToFind" == */  ]]; then
         echo "ENDS WITH /"
         RecoverOptions+=(" --target=$Destination/$Basename ")
       fi
-      eval avtar -x ${RecoverOptions[@]} $File --labelnum=$BackupToRestore
+      eval avtar -x ${RecoverOptions[@]} $FileToRestore --labelnum=$BackupToRestore
   else
       echo "Searched ${#BackupIds[@]} backups, but could not find $File. Aborting restore." 
       exit 5
